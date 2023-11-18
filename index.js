c3a61de75db7d437e8491dde03478c344f0c4b74
AnthonyIrudhayaAakash <anthonyaakash41@gmail.com>
2023-11-18 22:12:10 +0530

    Assignment-02

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1",{id:"heading_hello"},"Hello guys from React and React-DOM; We are going to rock this world");
+const heading = React.createElement("h1",{id:"heading_hello"},"Hello guys from React and React-DOM;git");
 
 const container = ReactDOM.createRoot(document.getElementById("container"));
 const alt_heading = React.createElement("p",{id:"para"}, "I'm Anthony Aakash")
