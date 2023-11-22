910615992b23d1380bc22591879634c27050d0d9
AnthonyIrudhayaAakash <anthonyaakash41@gmail.com>
2023-11-22 23:44:47 +0530

    Assignment-3

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const heading = React.createElement("h1",{id:"heading_hello"},"Hello World");
+import React from "react";
+/*The react-dom/client module is specifically designed for client-side rendering.
+ so we are using client here*/
+import ReactDOM from "react-dom/client"
+import image from "./Screenshot (1)-modified.png"
 
-const container = ReactDOM.createRoot(document.getElementById("container"));
+import uicon from "./icons8-user-35.png"
 
-container.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("container"));
+
+const Header = () => {
+    return (
+        <div className="containerJS" id="containerJS">
+            <img src={image} height= "100"></img>
+            <form className="search-form">
+                <label for="search"></label>
+                <input type="search" id="searchbar" placeholder="Search for Something"></input>
+                
+            </form>
+            <img className="btn-1" src={uicon} width="40"></img>
+        </div>
+    )
+}
+root.render(<Header/>);
