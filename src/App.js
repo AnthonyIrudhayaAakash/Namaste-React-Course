de6ea7070950c7d5d1aebb16a82918461f02148c
AnthonyIrudhayaAakash <anthonyaakash41@gmail.com>
2024-01-16 18:48:00 +0530

    ASSIGNMENT-07

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-/*The react-dom/client module is specifically designed for client-side rendering.
- so we are using client here*/
 import ReactDOM from "react-dom/client"
 import Header from "./Components/Header";
-import Body from "./Components/Body"
-import Offercards from "./Components/Offercards";
-// import Filter from "./Components/Filter";
+import About from "./Components/About";
+import Contact_us from "./Components/Contact_us";
+import Error from "./Components/Error";
+import Searchbody from "./Components/Searchbody";
+import RestaurentCards from "./Components/RestaurentCards";
+import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+import RestaurentMenu from "./Components/RestaurentMenu";
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("container"));
@@ -13,15 +17,39 @@ const MasterLayout = () =>{
     return (
         <div className="MasterContainer">
             <Header/>
-            
-            
-            {/* <div className="Bodycontainer">
-               
-               <Body/>
-            </div> */}
+            <Outlet/>
         </div>
     )
 }
 
+const AppRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <MasterLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Searchbody/>
+            },
+            {
+                path:"/about",
+                element: <About/>,
+            },
+            {
+                path:"/contact",
+                element: <Contact_us/>
+            },
+            {
+                path:"/restaurentmenu/:resid",
+                element: <RestaurentMenu/>
+            }
+        ],
+        errorElement: <Error/>
+    },
 
-root.render(<MasterLayout/>);
+])
+
+
+
+
+root.render(<RouterProvider router={AppRouter}/>);
